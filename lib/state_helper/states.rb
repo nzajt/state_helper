b70ead9d2b236b55dfa4221626a81435f20d5d1d
Nate <ndaily@netmediagroup.com>
2016-04-04 11:28:31 -0600

    add Guam and Virgin Islands

--- lib/state_helper/states.rb
+++ lib/state_helper/states.rb
@@ -1,78 +1,80 @@
 module StateHelper
-	class States
-		attr_reader :states
+  class States
+    attr_reader :states
 
-		def initialize
-			@states = {
-				'Alabama' => 'AL',
-				'Alaska' => 'AK',
-				'Arizona' => 'AZ',
-				'Arkansas' => 'AR',
-				'California' => 'CA',
-				'Colorado' => 'CO',
-				'Connecticut' => 'CT',
-				'Delaware' => 'DE',
-				'Florida' => 'FL',
-				'Georgia' => 'GA',
-				'Hawaii' => 'HI',
-				'Idaho' => 'ID',
-				'Illinois' => 'IL',
-				'Indiana' => 'IN',
-				'Iowa' => 'IA',
-				'Kansas' => 'KS',
-				'Kentucky' => 'KY',
-				'Louisiana' => 'LA',
-				'Maine' => 'ME',
-				'Maryland' => 'MD',
-				'Massachusetts' => 'MA',
-				'Michigan' => 'MI',
-				'Minnesota' => 'MN',
-				'Mississippi' => 'MS',
-				'Missouri' => 'MO',
-				'Montana' => 'MT',
-				'Nebraska' => 'NE',
-				'Nevada' => 'NV',
-				'New Hampshire' => 'NH',
-				'New Jersey' => 'NJ',
-				'New Mexico' => 'NM',
-				'New York' => 'NY',
-				'North Carolina' => 'NC',
-				'North Dakota' => 'ND',
-				'Ohio' => 'OH',
-				'Oklahoma' => 'OK',
-				'Oregon' => 'OR',
-				'Pennsylvania' => 'PA',
-				'Rhode Island' => 'RI',
-				'South Carolina' => 'SC',
-				'South Dakota' => 'SD',
-				'Tennessee' => 'TN',
-				'Texas' => 'TX',
-				'Utah' => 'UT',
-				'Vermont' => 'VT',
-				'Virginia' => 'VA',
-				'Washington' => 'WA',
-				'West Virginia' => 'WV',
-				'Wisconsin' => 'WI',
-				'Wyoming' => 'WY',
-				'Washington DC' => 'DC'
-			}
-		end
+    def initialize
+      @states = {
+        'Alabama' => 'AL',
+        'Alaska' => 'AK',
+        'Arizona' => 'AZ',
+        'Arkansas' => 'AR',
+        'California' => 'CA',
+        'Colorado' => 'CO',
+        'Connecticut' => 'CT',
+        'Delaware' => 'DE',
+        'Florida' => 'FL',
+        'Georgia' => 'GA',
+        'Guam' => 'GU',
+        'Hawaii' => 'HI',
+        'Idaho' => 'ID',
+        'Illinois' => 'IL',
+        'Indiana' => 'IN',
+        'Iowa' => 'IA',
+        'Kansas' => 'KS',
+        'Kentucky' => 'KY',
+        'Louisiana' => 'LA',
+        'Maine' => 'ME',
+        'Maryland' => 'MD',
+        'Massachusetts' => 'MA',
+        'Michigan' => 'MI',
+        'Minnesota' => 'MN',
+        'Mississippi' => 'MS',
+        'Missouri' => 'MO',
+        'Montana' => 'MT',
+        'Nebraska' => 'NE',
+        'Nevada' => 'NV',
+        'New Hampshire' => 'NH',
+        'New Jersey' => 'NJ',
+        'New Mexico' => 'NM',
+        'New York' => 'NY',
+        'North Carolina' => 'NC',
+        'North Dakota' => 'ND',
+        'Ohio' => 'OH',
+        'Oklahoma' => 'OK',
+        'Oregon' => 'OR',
+        'Pennsylvania' => 'PA',
+        'Rhode Island' => 'RI',
+        'South Carolina' => 'SC',
+        'South Dakota' => 'SD',
+        'Tennessee' => 'TN',
+        'Texas' => 'TX',
+        'Utah' => 'UT',
+        'Vermont' => 'VT',
+        'Virginia' => 'VA',
+        'Virgin Islands' => 'VI',
+        'Washington' => 'WA',
+        'West Virginia' => 'WV',
+        'Wisconsin' => 'WI',
+        'Wyoming' => 'WY',
+        'Washington DC' => 'DC'
+      }
+    end
 
-		def codes
-			@states.values
-		end
+    def codes
+      @states.values
+    end
 
-		def names
-			@states.keys
-		end
+    def names
+      @states.keys
+    end
 
-		def get_name_by_code code
-			@states.select {|key, value| value == code.upcase}.first.first
-		end
+    def get_name_by_code code
+      @states.select {|key, value| value == code.upcase}.first.first
+    end
 
-		def get_code_by_name name
-			@states[name.titleize]
-		end
+    def get_code_by_name name
+      @states[name.titleize]
+    end
 
-	end
+  end
 end
--- lib/state_helper/version.rb
+++ lib/state_helper/version.rb
@@ -1,3 +1,3 @@
 module StateHelper
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 end
